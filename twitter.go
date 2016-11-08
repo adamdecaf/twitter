311582a258e4130b53ed5ac349632585970be236
Adam Shannon <adamkshannon@gmail.com>
2016-11-08 17:07:55 -0600

    Adding root Tweet and User models

---
+++ twitter.go
@@ -0,0 +1,23 @@
+package twitter
+
+import (
+	"time"
+)
+
+// `Tweet` represents the data we care about from tweets
+type Tweet struct {
+	Id string
+	UserId string
+	Text string
+	CreatedAt time.Time
+}
+
+// `User` contains information on the account which creates tweets.
+type User struct {
+	Id string
+	Name string
+	ScreenName string
+	CreatedAt time.Time
+}
+
+// todo: Versioning on these models?
