c2de355474303cc03b69012cb1a2887dad95dfc2
Adam Shannon <adamkshannon@gmail.com>
2016-11-08 17:09:48 -0600

    ingest: Connect it all together under main method

--- ingest/main.go
+++ ingest/main.go
@@ -1,5 +1,45 @@
 package ingest
 
-func main() {
+import (
+	"github.com/ChimeraCoder/anaconda"
+	"log"
+	"os"
+)
 
+func main() {
+	config := NewConfig()
+	credentials, err := NewCredentials()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	if credentials == nil {
+		log.Println("No credentials found")
+		os.Exit(1)
+	}
+
+	client, err := NewClient(*credentials, config)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	// Read off tweets forever, die if something panics.
+	// todo: leaks underlying twitter library
+	for {
+		item := <- client.Events
+		tweet, ok := item.(anaconda.Tweet)
+		if ok {
+			go parseAndStore(tweet)
+		}
+	}
+}
+
+func parseAndStore(tweet anaconda.Tweet) {
+	t, _ := parse(tweet)
+
+	// store tweet
+	if t != nil {
+		storeTweet(*t)
+	}
 }
