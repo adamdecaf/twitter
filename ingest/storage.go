a0f01e79ad4700fbe6c465df81bbbbf0f228aa43
Adam Shannon <adamkshannon@gmail.com>
2016-11-14 16:15:25 -0600

    ingest: Setup baseline impl for batched storage

--- ingest/storage.go
+++ ingest/storage.go
@@ -4,6 +4,27 @@ import (
 	"github.com/adamdecaf/twitter"
 )
 
-func storeTweet(tweet twitter.Tweet) {
+// An layer for storing tweets. It expects a batch of tweets instead of single tweets
+// at a time.
+type Storage interface {
+	Store([]twitter.Tweet) error
+}
 
+// `kafkaStorage` writes batches of tweets to kafka.
+type kafkaStorage struct {
+	Storage
+
+	Brokers []string
+}
+
+// `NewKafkaStorage`...
+func NewKafkaStorage(brokers []string) Storage {
+	return kafkaStorage{
+		Brokers: brokers,
+	}
+}
+
+// `Store`...
+func (k kafkaStorage) Store(batch []twitter.Tweet) error {
+	return nil
 }
