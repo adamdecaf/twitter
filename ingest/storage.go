1ef2b5131f0464d647e63e77e8fdeb76d57c88c8
Adam Shannon <adamkshannon@gmail.com>
2016-11-14 16:46:53 -0600

    ingest: Write tweets into kafka

--- ingest/storage.go
+++ ingest/storage.go
@@ -4,6 +4,11 @@ import (
 	"github.com/adamdecaf/twitter"
 	"github.com/Shopify/sarama"
 	"log"
+	"time"
+)
+
+var (
+	TweetsKafkaTopic = "tweets.v1"
 )
 
 // An layer for storing tweets. It expects a batch of tweets instead of single tweets
@@ -24,12 +29,38 @@ type kafkaStorage struct {
 
 // `NewKafkaStorage`...
 func NewKafkaStorage(brokers []string) Storage {
+	config := sarama.NewConfig()
+	config.Net.TLS.Enable = false
+	config.Producer.RequiredAcks = sarama.WaitForLocal       // Only wait for the leader to ack
+	config.Producer.Compression = sarama.CompressionSnappy   // Compress messages
+	config.Producer.Flush.Frequency = 500 * time.Millisecond // Flush batches every 500ms
+
+	producer, err := sarama.NewAsyncProducer(brokers, config)
+	if err != nil {
+		log.Fatalln("Failed to start Sarama producer:", err)
+	}
+
 	return kafkaStorage{
 		Brokers: brokers,
+		produder: producer,
 	}
 }
 
-// `Store`...
+// `Store` sends messages off to kafka. It assumes they're formatted properly after calling
+// `Serialize()` on the passed in values.
 func (k kafkaStorage) Store(batch []twitter.Tweet) error {
+	for _, tweet := range batch {
+		b := tweet.Serialize()
+		if b == nil {
+			// Skip any tweets that don't properly serialize
+			continue
+		}
+
+		k.produder.Input() <- &sarama.ProducerMessage{
+			Topic: TweetsKafkaTopic,
+			Key: sarama.StringEncoder(tweet.Id),
+			Value: sarama.ByteEncoder(*b),
+		}
+	}
 	return nil
 }
--- twitter.go
+++ twitter.go
@@ -1,15 +1,25 @@
 package twitter
 
 import (
+	"encoding/json"
 	"time"
 )
 
 // `Tweet` represents the data we care about from tweets
 type Tweet struct {
-	Id string
-	UserId string
-	Text string
-	CreatedAt time.Time
+	Id string `json:"id"`
+	UserId string `json:"userId"`
+	Text string `json:"text"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+// todo
+func (t Tweet) Serialize() *[]byte {
+	b, err := json.Marshal(t)
+	if err != nil {
+		return nil
+	}
+	return &b
 }
 
 // `User` contains information on the account which creates tweets.
@@ -18,6 +28,15 @@ type User struct {
 	Name string
 	ScreenName string
 	CreatedAt time.Time
+}
+
+// todo
+func (u User) Serialize() *[]byte {
+	b, err := json.Marshal(u)
+	if err != nil {
+		return nil
+	}
+	return &b
 }
 
 // todo: Versioning on these models?
